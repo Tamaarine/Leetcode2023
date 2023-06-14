23d31c6c082b4c43edc34be39fd20e76bbad6f46
Tamaarine <irebootplaygt@gmail.com>
2023-06-14 00:54:02 +0000

    Use built-in counters for keeping track of the numbers, and then just if-statement cases for those encountered numbers.

---
+++ array/two_sum.py
@@ -0,0 +1,28 @@
+from typing import *
+from collections import Counter
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for index_i in range(len(nums)):
+            for index_j in range(index_i + 1, len(nums)):
+                if target - nums[index_i] == nums[index_j]:
+                    return [index_i, index_j]
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        c = Counter(nums)
+        for index in range(len(nums)):
+            c[nums[index]] -= 1
+            needed = target - nums[index]
+            if needed in c and c[needed] > 0:
+                if needed == nums[index]:
+                    return [index, nums.index(needed, index + 1)]
+                return [index, nums.index(needed)]
+            c[nums[index]] += 1
+                
+
+s = Solution2()
+print(s.twoSum([1, 2, 3, 4, 5, 6, 7, 8], 10))
+print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([3, 2, 4], 6))
+print(s.twoSum([3, 3], 6))
