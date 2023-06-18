0096345eab792f1a9be18c5fb0d79ca153cbfddc
Tamaarine <irebootplaygt@gmail.com>
2023-06-18 19:16:02 +0000

    Standard binary search

---
+++ binary_search/binary_search.py
@@ -0,0 +1,19 @@
+from typing import *
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (right - left) // 2 + left
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+s = Solution()
+print(s.search([1, 2, 3, 4, 5, 6, 10, 20, 23], 23))
